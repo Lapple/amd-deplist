046fde100502bac2fbcb2e3e89fbb8917ca0bd7b
Aziz Yuldoshev <aziz.yuldoshev@booking.com>
2015-04-18 12:04:30 +0200

    Allow multiple main files

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ var path = require('path');
 var EOL = require('os').EOL;
 
 var _ = require('lodash');
+var Promise = require('bluebird')
 var amodroTrace = require('amodro-trace');
 var cjsTransform = require('amodro-trace/read/cjs');
 
 var removeNamespace = require('./remove-namespace');
 var moduleId = require('./module-id');
 
-function trace(file, options) {
+function trace(files, options) {
+    Promise
+        .all(
+            files.map(
+                _.partial(traceFile, _, options)))
+        .then(function(rs) {
+            console.log(
+                _(rs)
+                    .flatten()
+                    .unique()
+                    .value()
+                    .join(EOL));
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
+}
+
+function traceFile(file, options) {
     var root = options.rootdir;
     var paths = options.paths;
 
-    amodroTrace(
+    return amodroTrace(
         {
             rootDir: root,
             id: moduleId(file, root),
@@ -28,18 +47,13 @@ function trace(file, options) {
             paths: paths
         }
     ).then(function(r) {
-        console.log(
-            _(r.traced)
-                .pluck('path')
-                .unique()
-                .map(
-                    path.relative.bind(
-                        path,
-                        root))
-                .value()
-                .join(EOL));
-    }).catch(function(error) {
-        console.error(error);
+        return _(r.traced)
+            .pluck('path')
+            .map(
+                path.relative.bind(
+                    path,
+                    root))
+            .value();
     });
 }
 
